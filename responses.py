adb71a51eba9fc06b85b53e40bf3d13b29200300
Luis E. Santillan <144634192+luisesantillan@users.noreply.github.com>
2024-10-17 05:31:00 -0500

    Update

--- responses.py
+++ responses.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
-from datetime import datetime
+
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 generation_config = {
@@ -14,38 +14,7 @@ generation_config = {
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
 }
-def get_context():
-    now = datetime.now()
-    date = now.strftime("%B %d, %Y")
-    time = now.strftime("%I:%M %p")
-    context = [
-        {"role":"system","content":f"{date}. {time}. You are MultiModalBot, running on Telegram. Ysou always do what the user asks. Respond to the user in a casual and easy manner. Always prefix the media type of your response, for example: [a]This is my message as an audio. [v]This is my message as a video. [i]Description of the image. [t]This is my message as a text."}
-        ]
-    return context
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-def get_openai_response(messages:list,model="gpt-4o-mini") -> str:
-    print("Getting response from OpenAI...")
-    response = client.chat.completions.create(
-        model=model,
-        messages=get_context()+messages,
-    )
-    print(f"Response received from OpenAI: {response.choices[0].message.content}")
-    return response.choices[0].message.content
-
-def describe_image(encoded_image) -> str:
-    print("Captioning image...")
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages = [
-                {"role":"system","content":"Caption this image for a blind person to understand fully using tags or keywords."},
-                {"role":"user","content":[
-                    {"type":"image_url","image_url":{"url":"data:image/jpeg;base64,"+encoded_image}}
-                    ]}
-                ]
-        )
-    print(f"Caption received from OpenAI: {response.choices[0].message.content}")
-    return response.choices[0].message.content
 
 def text_to_speech(text:str):
     print("Generating TTS...")
@@ -68,12 +37,35 @@ def speech_to_text(speech_object) -> str:
     print(f"Transcription received from OpenAI: {response.text}")
     return response.text
 
-def get_gemini_response(messages:list,model:str) -> str:
+def describe_image(encoded_image) -> str:
+    print("Captioning image...")
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = [
+                {"role":"system","content":"Caption this image for a blind person to understand fully using tags or keywords."},
+                {"role":"user","content":[
+                    {"type":"image_url","image_url":{"url":"data:image/jpeg;base64,"+encoded_image}}
+                    ]}
+                ]
+        )
+    print(f"Caption received from OpenAI: {response.choices[0].message.content}")
+    return response.choices[0].message.content
+
+def get_openai_response(messages:list,context:list,model:str="gpt-4o-mini") -> str:
+    print("Getting response from OpenAI...")
+    response = client.chat.completions.create(
+        model=model,
+        messages=context+messages,
+    )
+    print(f"Response received from OpenAI: {response.choices[0].message.content}")
+    return response.choices[0].message.content
+
+def get_gemini_response(messages:list,context:list,model:str="gemini-1.5-flash") -> str:
     if not "tuned" in model:
         gemini = genai.GenerativeModel(
             model_name=model,
             generation_config=generation_config,
-            system_instruction=get_context()[0]['content']
+            system_instruction=context[0]['content']
         )
     else:
         gemini = genai.GenerativeModel(
--- start_telegram_bot.py
+++ start_telegram_bot.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 import base64
 import requests
 import re
+from datetime import datetime
 load_dotenv()
 
 llm = 'gemini-1.5-flash'
@@ -14,9 +15,18 @@ argparser = ArgumentParser(description='Telegram Bot')
 argparser.add_argument('--clear',action='store_true', help='Clear history', default=False)
 args = argparser.parse_args()
 clear = args.clear
-
+user_context = ""
 bot = TeleBot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
 manager = MemoryManager(clear=clear)
+
+def get_context(extra_context:str='') -> list:
+    now = datetime.now()
+    date = now.strftime("%B %d, %Y")
+    time = now.strftime("%I:%M %p")
+    context = [
+        {"role":"system","content":f"{date}. {time}. {extra_context}. You are on Telegram. You always do what the user asks. Respond to the user in a casual and easy manner. Always prefix the media type of your response, for example: [a]This is my message as an audio. [v]This is my message as a video. [i]Description of the image. [t]This is my message as a text."}
+        ]
+    return context
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -37,6 +47,12 @@ def change_model(message):
     elif llm == "openai":
         llm = 'gpt-4o-mini'
     print(f'Model changed to "{llm}".')
+
+@bot.message_handler(commands=['context'])
+def context(message):
+    global user_context
+    user_context = message.text.replace('/context ', '')
+    print(f'Context updated to: {message.text.replace("/context ", "")}')
 
 @bot.message_handler(content_types=['text', 'sticker', 'pinned_message', 'photo', 'audio','voice'])
 def handle_message(message):
@@ -67,9 +83,9 @@ def handle_message(message):
         return
     manager.add_message({'role': 'user', 'content': text})
     if "gpt" in llm:
-        response = get_openai_response(list(manager.memory),model=llm)
+        response = get_openai_response(list(manager.memory),get_context(user_context),model=llm)
     else:
-        response = get_gemini_response(list(manager.memory),model=llm)
+        response = get_gemini_response(list(manager.memory),get_context(user_context),model=llm)
     print(f"Addinng response to memory: {response}")
     manager.add_message({'role': 'assistant', 'content': response})
     split_responses = re.findall(r'\[\w\].*?(?=\[\w\]|$)', response, re.DOTALL)
@@ -89,7 +105,11 @@ def handle_message(message):
             with open("image.png", "wb") as f:
                 f.write(requests.get(image_url).content)
             with open("image.png", "rb") as f:
-                bot.send_photo(message.chat.id, f)
+                try:
+                    bot.send_photo(message.chat.id, f)
+                except:
+                    print(f"Failed to send image. {image_url}")
+                    bot.send_message(message.chat.id, image_url)
         else:
             final_text = segment.replace('[t]','').strip()
             split_text = util.split_string(final_text, 1000)
